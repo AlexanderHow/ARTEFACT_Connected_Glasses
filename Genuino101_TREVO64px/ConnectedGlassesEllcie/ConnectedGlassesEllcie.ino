c27343dc88f284cfccea289457cded88dfa661a6
Alexander <alexandre.howard06@gmail.com>
2019-06-25 15:44:00 +0200

    setup and loop
    Setup de la carte et fonction qui loop sur la carte

--- Genuino101_TREVO64px/ConnectedGlassesEllcie/ConnectedGlassesEllcie.ino
+++ Genuino101_TREVO64px/ConnectedGlassesEllcie/ConnectedGlassesEllcie.ino
@@ -29,7 +29,7 @@ void parse_depth_array(){
         indexDepth ++;
       }
     }else{
-      //rest of the trame, last 8 bytes are the CRC32 checksum  
+      //rest of the trame, last 8 bytes are the CRC32 checksum or wrong id
     }
     byteRead++;
   }
@@ -40,10 +40,11 @@ void parse_depth_array_mock_up(){
   for(int i = 0; i < 64; ++i){
     depth_array[i] = random(0,255);  
   }
+  hasDepthMapToSend = true;
 }
 
 void send_depth_array(){
-  char toSend[20] = {0};
+  unsigned char toSend[20] = {0};
   toSend[0] = 0x11;
   toSend[2] = 8;
   for(int j = 0; j < 8; ++j){
@@ -54,14 +55,38 @@ void send_depth_array(){
     trame.setValue(toSend, 20);
     delay(100);
   }
+  hasDepthMapToSend = false;
 }
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  delay(100);
+  
+  ///TERA EVO 64px UART
+  //Serial1.begin(115200);
+  //Serial1.write(0x0011024C); //Distance mode
+  delay(100);
+  
+  ///BLE
+  blePeripheral.setLocalName("ARTEFACT");
+  blePeripheral.setAdvertisedServiceUuid(glassesService.uuid());
+  blePeripheral.addAttribute(glassesService);
+  blePeripheral.addAttribute(trame);
+  blePeripheral.begin();
+  delay(100);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  BLECentral central = blePeripheral.central();
+  if (central) {
+    while (central.connected()) {
+      if(hasDepthMapToSend == true){
+         send_depth_array();
+      }else{
+         parse_depth_array_mock_up(); //TO CHANGE WHEN WE HAVE THE ACTUAL SENSOR
+      }
+      delay(500);
+    }
+  }
 
 }
