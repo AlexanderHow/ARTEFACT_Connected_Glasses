241a54d8242e7897e50f15350f503c90b1395750
Alexander <alexandre.howard06@gmail.com>
2019-07-18 17:02:31 +0200

    Clean code
    Refactor de code pour les cibles embarqué en vue de quelques optimisations

--- STM32_to_Genuino101_TREVO64px/Genuino_ConnectedGlasses/Genuino_ConnectedGlasses.ino
+++ STM32_to_Genuino101_TREVO64px/Genuino_ConnectedGlasses/Genuino_ConnectedGlasses.ino
@@ -6,64 +6,51 @@ BLECharacteristic trame("b60ba360-5c16-4ddb-bdb2-d134fa42bfbc", BLERead | BLENot
 
 uint8_t * depth_array = NULL;
 int indexDepth = 0;
-bool startSetUpInitParams = false;
-bool setUpDone = false;
-bool startFrame = false;
+bool initialDataStream = false;
 int numLine = -1;
 int numCol = -1;
 
+//Sending one line of the depth map that changed as ( 0x 11 SN LL XX XX XX XX ... 00 or XX) size = 20 bytes
+//with SN : Sequential number or index of the line, LL : Line length, XX : Depth data, 00 : Padding to make a 20 bytes long trame
 void sendDepthArrayLine(int indexLine){
-  //Sending one line of the depth map that changed as ( 0x 11 SN LL XX XX XX XX ... 00 or XX) size = 20 bytes
-  //with SN : Sequential number or index of the line, LL : Line length, XX : Depth data, 00 : Padding to make a 20 bytes long trame
-  if(numLine > 0 && numCol > 0 && indexLine < numLine && numCol < 17){
+  if(indexLine < numLine && numCol < 17){
     unsigned char toSend[20] = {0};
     toSend[0] = 17; //0x11
-    Serial.print(17);Serial.print(" ");
     toSend[1] = (char)indexLine;    
-    Serial.print(indexLine);Serial.print(" ");
     toSend[2] = numCol;
-    Serial.print(numCol);Serial.print(" ");
     for(int k = 0; k < numCol; ++k){
-      toSend[k+3] = (char)depth_array[8*indexLine+k];
-      Serial.print(depth_array[8*indexLine+k]);Serial.print(" ");  
+      toSend[k+3] = (char)depth_array[8*indexLine+k];  
     }
-    //trame.setValue(toSend, 20);
-    Serial.println();
-    delay(100);
+    trame.setValue(toSend, 20);
+    delay(10);
   }
 }
 
-void setUpInitialParams(uint8_t input){
-  if(startSetUpInitParams == false){
-    if(input == 255){ //start of initial data trame
-      startSetUpInitParams = true;  
+void setUpInitialParams(uint8_t recv){
+  if( numLine <= 0){ 
+    if( initialDataStream == true){ 
+      numLine = (recv > 0) ? recv : -1;
     }
-  }else{
-    //setting the size of the matrix
-    if(numLine == -1){ 
-      numLine = (input > 0) ? input : -1;
-    }else{
-      numCol = (input > 0) ? input : -1;
-    }
-    //allocating the matrix
-    if(numLine > 0 && numCol > 0){ 
+  }else if( numCol <= 0){ 
+    if( initialDataStream == true){ 
+      numCol = (recv > 0) ? recv : -1;
       if(depth_array != NULL){
         free(depth_array);  
       }
       depth_array = (uint8_t*)calloc(numLine*numCol, sizeof(uint8_t));
-      setUpDone = true;
+      initialDataStream = false;
     }
   }
 }
 
 void setup() {
   //BLE set up
-  /*blePeripheral.setLocalName("ARTEFACT");
+  blePeripheral.setLocalName("ARTEFACT");
   blePeripheral.setAdvertisedServiceUuid(glassesService.uuid());
   blePeripheral.addAttribute(glassesService);
   blePeripheral.addAttribute(trame);
   blePeripheral.begin(); 
-  delay(80);*/
+  delay(80);
 
   //DEBUG
   Serial.begin(115200);
@@ -73,41 +60,39 @@ void setup() {
   Serial1.begin(115200);
   delay(80);
   while(!Serial1){;}
-  delay(100);
 }
 
 void loop() {
   uint8_t recv;
-  while(setUpDone == false){ //INITIAL DATA : size of the matrix as (0x FF XX XX)
-    if(Serial1.available() > 0){
-      recv = (uint8_t)Serial1.read();
-      setUpInitialParams(recv);  
+  BLECentral central = blePeripheral.central();
+  if(Serial1.available() > 0){
+    recv = (uint8_t)Serial1.read();
+
+    if(recv == 255){ //Start of the stream about the sizes of the depth matrix
+      initialDataStream = true;
     }
-  }
-  
-  //START BLE AND RECEVING DATA FROM SENSOR
-  //BLECentral central = blePeripheral.central();
-  //if (central) {
-    //while (central.connected()) {
-      if(Serial1.available() > 0){
-        recv = (uint8_t)Serial1.read(); 
-        if(recv == 0){ //start trame
-          indexDepth = 0;  
-          startFrame = true;
-        }else if(startFrame == true){ //depth data
-          depth_array[indexDepth] = recv;
-          indexDepth = (indexDepth + 1);
-          if(indexDepth > 0 && (indexDepth%numCol) == 0){ //new line of the depth map
-            sendDepthArrayLine((indexDepth-1)/numCol);
-          }
-          indexDepth = indexDepth % (numLine*numCol);
-          //reset
-          if(indexDepth == 0){
-            startFrame = false;
-            //Serial1.flush();  
+    
+    if( numLine > 0 && numCol > 0){ //Stream about depth data, starting by a 0 and followed by 64 values
+      if( recv == 0){ //Start of depth stream
+        indexDepth = 0;
+      }else{ //Depth data
+        depth_array[indexDepth] = recv;
+        indexDepth++;
+        
+        if(indexDepth != 0 && (indexDepth%numCol) == 0){ //Start of a new line in the depth matrix
+          int indexLine = (indexDepth-1)/numLine;
+          if( central && central.connected()){ //Send the line through BLE
+            sendDepthArrayLine(indexLine);
           }
         }
+
+        if(indexDepth >= (numLine*numCol)){ //Security to not overflow the matrix if some pieces of the data stream were missed
+          indexDepth = (numLine*numCol) - 1;
+        }
+        
       }
-    //}
-  //}
+    }else{ //Stream about the sizes of the depth matrix
+      setUpInitialParams(recv);
+    }
+  }
 }
--- STM32_to_Genuino101_TREVO64px/STM32_ConnectedGlasses/STM32_ConnectedGlasses.ino
+++ STM32_to_Genuino101_TREVO64px/STM32_ConnectedGlasses/STM32_ConnectedGlasses.ino
@@ -2,66 +2,21 @@ int NUMLINE = 8;
 int NUMCOL = 8;
 
 HardwareSerial Serial1(PA10, PA9); //rx, tx from sensor
-HardwareSerial Serial4(PA1, PA0); //rx, tx from sensor
+HardwareSerial Serial4(PA1, PA0); //rx, tx to main board
 
-int depth_array[64] = {0};
-byte frame[137] = {0}; //1 byte header + 64*2 bytes of data + 8 bytes of CRC32
+//1 byte header + 64*2 bytes of data + 8 bytes of CRC32
+//int matrix[64] = {0}:
+//int indexDepth = 0;
+byte crc32[8] = {0};
 int indexFrame = 0;
 int indexCRC = 0;
-bool hasDepthMapToSend = false;
+byte highRecvByte = 0;
+byte lowRecvByte = 0;
 bool paddingEnded = false;
 bool setupDone = false;
 
-//Parsing the trame to the depth matrix, distances between 0 to 5000mm mapped to range 1-254
-void parseEvo(){
-  byte header;
-  uint16_t distance;
-  header = frame[0];
-  if(header == 0x11){
-    for(int i = 1; i < 65; ++i){
-      distance = ((frame[2*i-1] & 0x1F) << 7) | (frame[2*i] & 0x7F);
-      // ou distance = (frame[2*i-1]<<8) + frame[2*i];
-      distance = (distance > 5000) ? 5000 : distance;
-      distance = map(distance, 0, 5000, 1, 254);
-      depth_array[i-1] = distance;
-    }
-    //crc32 only on the 4 lower bits of each
-    /*for(int j = 129 ; j < 137; ++j){
-      uint8_t crc = frame[j] (& 0x0F);
-    }*/
-    hasDepthMapToSend = true;
-   }  
-}
-
-void sendSSIDepth(){
-  Serial4.write(0);
-  delay(5);
-  for(int i = 0; i < 64; ++i){
-    Serial4.write(depth_array[i]);
-    delay(5);
-  }
-  hasDepthMapToSend = false;
-}
-
-void setup() { 
-  /*TERA EVO 64px UART*/
-  Serial1.begin(3000000);
-  delay(80);
-  Serial1.write("\x00\x11\x02\x4C"); //distance mode
-  delay(80);
-  /*Serial1.write("\x00\x21\x02\xB5"); //fast mode
-  delay(100);*/
-
-  //Serial communication with main board
-  //Set the baud rate of your needs
-  Serial4.begin(115200);
-  delay(80);
-  while(!Serial4){;}
-}
-
-void loop() {
-  byte recv;
-  if(setupDone == false){ //TRANSMIT INITIAL DATA : size of the depth matrix
+void sendInitialData(){
+  if(Serial4){ 
     Serial4.write(255);
     delay(5);
     Serial4.write(NUMLINE);
@@ -69,45 +24,83 @@ void loop() {
     Serial4.write(NUMCOL);
     delay(5);
     setupDone = true;
-  }else{ //SENDING THE DEPTH MATRIX TO MAIN BOARD
-    if(indexFrame == 129 && indexCRC == 8){
-      parseEvo();
-      if(hasDepthMapToSend == true){
-         sendSSIDepth();
-      }  
-      indexFrame = 0; 
-      indexCRC = 0;
-      paddingEnded = false; 
-    }else{ //READING DATA FROM SENSOR 
-      while(Serial1.available()>0){
-        recv = byte(Serial1.read());
-        if(indexFrame == 0){ //HEADER
-          if(recv == 0x11){
-            frame[indexFrame] = recv;
-            indexFrame++;
-          }
-        }
-        else if(indexFrame > 0 && indexFrame < 129){ //DISTANCE DATA
-          frame[indexFrame] = recv;
-          indexFrame++;
-        }
-        else{ //PADDING AND CRC32
-          if(paddingEnded == true){
-            if(indexCRC < 8){
-              frame[indexFrame + indexCRC] = recv;
-              indexCRC++;
-            }else{
-              break;
-            }
-          }else{
-            if(recv != 128){ //0x80 = padding
-              frame[indexFrame + indexCRC] = recv;
-              indexCRC++;
-              paddingEnded = true;
-            }
-          }
-        }
-      }
+  }
+}
+
+int getDistance(){ 
+  int distance = ((highRecvByte & 0x1F) << 7) | (lowRecvByte & 0x7F);
+  distance = (distance > 5000) ? 5000 : distance;
+  distance = map(distance, 0, 5000, 1, 254);
+  return distance;
+}
+
+void headerAndDepthParser(byte recv){ 
+  if( recv == 17){ //start of the depth data stream
+    indexFrame = 1;
+    Serial4.write(0); //mark skip
+    delay(5);         //mark skip
+  }
+
+  if(indexFrame > 0){ //stream of data has strated, we got the 0x11 header
+    if((indexFrame%2) == 1){ //the high byte
+      highRecvByte = recv;
+    }else{  //the low byte
+      lowRecvByte = recv;
+      int d = getDistance();
+      Serial4.write(d);//mark skip
+      delay(5);        //mark skip
     }
+    indexFrame++;
+  }
+}
+
+void paddingAndCrcParser(byte recv){ 
+  if(paddingEnded == true){ //crc
+    crc32[indexCRC] = recv;
+    indexCRC++;
+    if(indexCRC > 7){ //end of crc32 and reset of the state of the stream reading process
+      indexCRC = 0;
+      indexFrame = 0;
+      paddingEnded = false;
+      //mark incr frame skipped
+      //TODO : check crc, if so replace in headerAndDepthParser "Serila4.write(d);" by "matrix[indexDepth]=d; indexDepth++;" and send all the matrix on Serial4 if the crc is ok
+    }
+  }else{ 
+    //padding = 0x80
+    if(recv != 128){ 
+      paddingEnded = true;
+      crc32[indexCRC] = recv;
+      indexCRC++;
+    }
+  }
+}
+
+void setup() { 
+  /*TERA EVO 64px UART*/
+  Serial1.begin(3000000);
+  while(!Serial1){;}
+  Serial1.write("\x00\x11\x02\x4C"); //distance mode
+  delay(5);
+
+  //Serial communication with main board
+  //Set the baud rate of your needs
+  Serial4.begin(115200);
+  while(!Serial4){;}
+  delay(5);
+}
+
+void loop() {
+  byte recv;
+  if(setupDone == false){ //TRANSMIT INITIAL DATA : size of the depth matrix
+    sendInitialData();
+  }else{ //SENDING THE DEPTH MATRIX TO MAIN BOARD
+    if(Serial1.available()>0){ 
+      recv = byte(Serial1.read());
+      if(indexFrame < 129){ //HEADER + DEPTH DATA
+        headerAndDepthParser(recv);
+      }else{ //PADDING + CRC32
+        paddingAndCrcParser(recv);
+      }
+    }   
   }
 }
